0159b817c57db4104f23a711c36352088cbea73d
dcustomer <82726795+dcustomer@users.noreply.github.com>
2022-01-06 03:49:14 -0800

    Delete task1 directory

--- task1/Views/content3.php
+++
@@ -1,6 +0,0 @@
-<div id="listItem">
-  <form action="addItem.php" method="post">
-   <input type="text" name="taskName" />
-   <input type="submit" name="add" value="EditItem" />
-  </form>
- </div>
--- task1/Views/editForm.php
+++
@@ -1,8 +0,0 @@
-<div id="listItem">
-  <form action="" method="post">
-   <input type="text" name="rewrite" value="<?php echo $editRow[0]['title']; ?>" />
-   <input type="hidden" name="id" value="<?php echo $editRow[0]['id'];  ?>" >
-   <input type="hidden" name="pagename" value="editItemRecords">
-   <input type="submit" name="edit" value="Edit" />
-  </form>
- </div>
